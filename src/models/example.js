2a2a4ff4fdac474e83f995cf2458ff50f1356db2
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-22 23:44:37 +0800

    fork 研究

--- src/models/example.js
+++ src/models/example.js
@@ -1,4 +1,6 @@
-
+// import {
+//   delay
+// } from '../services/forkService';
 export default {
 
   namespace: 'example',
@@ -7,12 +9,30 @@ export default {
 
   subscriptions: {
     setup({ dispatch, history }) {  // eslint-disable-line
+      history.listen(({pathname, query}) => {
+        if(pathname === '/about') {
+          dispatch({type: 'fetch'});
+        }
+      })
     },
   },
 
   effects: {
-    *fetch({ payload }, { call, put }) {  // eslint-disable-line
-      yield put({ type: 'save' });
+    *fetch({ payload }, { call, put, fork }) {  // eslint-disable-line
+      // console.log('delay==>', delay);
+      const delay = time => new Promise ( res => {
+        console.log('delaying...');
+        setTimeout(() => res('delay over!'), time);
+      })
+      // const sleep = function (time) {
+      //   return new Promise(function (resolve, reject) {
+      //     setTimeout(function () {
+      //       resolve();
+      //     }, time);
+      //   })
+      // };
+      const msg = yield call(delay, 5000);
+      console.log('msg==>over', msg);
     },
   },
 
