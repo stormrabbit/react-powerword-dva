9ae96562698a111e8b5f11269fb847e7b5d266f3
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-23 00:37:15 +0800

    搞清楚了 call 与 fork 的差别，以及稍微了解了 saga 的异步调用

--- src/models/example.js
+++ src/models/example.js
@@ -26,8 +26,12 @@ export default {
     *fetch({ payload }, { call, put, fork }) {  // eslint-disable-line
       // console.log('delay==>', delay);
       const delay = time => new Promise ( res => {
-        console.log('delaying...');
-        setTimeout(() => res('delay over!'), time);
+        console.log('call delaying...' + time);
+        setTimeout(() => res( 'call' + time + 'is delay over!'), time);
+      });
+      const delay2 = time => new Promise ( res => {
+        console.log('fork delaying...' + time);
+        setTimeout(() => res( 'fork' + time + 'is delay over!'), time);
       })
 
       // const tsk = yield fork(delay, 5000);
@@ -46,30 +50,38 @@ export default {
       //     }, time);
       //   })
       // };
-      // const arrs = [];
-      // for (let i =0;i< 4;i ++){
-      //   arrs.push(call(delay, (5+i) * 1000))
-      // }
+      const arrs = [];
+      for (let i =0;i< 4;i ++){
+        arrs.push(call(delay, (5+i) * 1000))
+      }
       const arrs2 = [];
       for (let i =0;i< 4;i ++){
-        arrs2.push(yield fork(delay, (5+i) * 1000))
+        arrs2.push(yield fork(delay2, (5+i) * 1000))
       }
-      const forkAction = (tsk) => {
-        return put({
-          type: 'takeFork',
-          payload: tsk
-        });
-      }
+      // const forkAction = (tsk) => {
+      //   return put({
+      //     type: 'takeFork',
+      //     payload: tsk
+      //   });
+      // }
 
-      for (let i in arrs2) {
-        yield forkAction(arrs2[i]);
-      }
-
-      const [tsk1] = arrs2;
-      if(tsk1.isRunning()) {
-        tsk1.cancel();
-      }
-      console.log(tsk1.isRunning())
+      // for (let i in arrs2) {
+      //   yield forkAction(arrs2[i]);
+      // }
+      const [cll1, cll2 ,cll3, cll4] = arrs;
+      console.log(yield cll1);
+      console.log(yield cll2);
+      console.log(yield cll3);
+      console.log(yield cll4);
+      const [tsk1,tsk2, tsk3, tsk4] = arrs2;
+      console.log(yield tsk1.done);
+      console.log(yield tsk2.done);
+      console.log(yield tsk3.done);
+      console.log(yield tsk4.done);    
+      // if(tsk1.isRunning()) {
+      //   tsk1.cancel();
+      // }
+      // console.log(tsk1.isRunning())
       // yield put({
       //   type: 'takeFork',
       //   payload: arrs2[1]
