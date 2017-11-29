07a2bffc52cbebfe4d6dfde888f6c7ed05a99e8a
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-29 23:45:46 +0800

    update

--- src/utils/zeroBuilder.js
+++ src/utils/zeroBuilder.js
@@ -3,17 +3,24 @@ import {
   Form
 } from 'antd';
 
-export const zeroBuilder = (formItems = [], buildItemFunc, formClass, styles) => {
+export const zeroBuilder = (buildItemFunc, formClass, formCreater = Form.create()) => {
+  const map = {};
 
-  const ZeroForm = props => {
+  const formItems = []
+  const builder = formItems => formCreater(props => {
+    map.form = props.form;
     return (
       <Form className={formClass} style={styles}>
         {
-          formItems.map(formItem => buildItemFunc(formItem, props.form))
+          buildItemFunc(props.form, formItems)
         }
       </Form>
     );
-  }
+  })
+  const formApi = () => map.form;
 
-  return Form.create()(ZeroForm)
+  return {
+    builder,
+    formApi
+  }
 }
