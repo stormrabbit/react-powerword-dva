128d8150eacc98028ca009b49b4cba9c9afe686f
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-12-01 23:57:46 +0800

    update

---
+++ src/containers/ModelDemo/ModelDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ModelDemo.css';
+
+function ModelDemo() {
+  return (
+    <div className={styles.normal}>
+      Hello, ModelDemo
+    </div>
+  );
+}
+
+export default ModelDemo;
