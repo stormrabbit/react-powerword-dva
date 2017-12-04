4fba838914819eeb09607290e2747e3abb634d86
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-12-04 23:33:55 +0800

    二维码研究

--- src/containers/HostPage/HostPage.js
+++ src/containers/HostPage/HostPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './HostPage.css';
+import QRCode from 'qrcode.react';
 
 function HostPage(props) {
   
   return (
     <div className={styles.normal} >
       Hello, HostPage
+      <div>
+        <QRCode value='https://github.com/' />
+      </div>
+      
     </div>
   );
 }
